98eea50688290a1b779b9eadecf3ac50d2710687
Marco Heuer <mheuer@gmail.com>
2023-01-18 16:38:56 -0600

    Hello world is working!

--- clock/main.s
+++ clock/main.s
@@ -48,129 +48,19 @@ button_press:
 
 
 
-  lda #$02
-  sta $00
-  lda #$00
-  sta $01
-  lda #$00
-  ldx #1
-  jsr rtc_read
-
-  lda $02
-  and #$80
-  ;beq .no_reset
-  ; Reset RTC to defaults, as it is stopped currently
-  lda #<rtc_default
-  sta $00
-  lda #>rtc_default
-  sta $01
-  lda #$00
-  ldx #8
-  jsr rtc_write
-
-.no_reset:
-; SETUP Timer2 to countdown 4096hz pulses on PB6
-  ;lda #(T1_ONESHOT|T2_COUNTPB6)
-  ;sta ACR
-
-date_loop:
-  ;lda #(1024 & $ff)
-  ;sta T2CL
-  ;lda #(1024 >> 8)
-  ;sta T2CH
-  ;cli
-  ;wai
-  ; (&buf[8] = 0x0002;)
-  lda #$02
-  sta $00
-  lda #$00
-  sta $01
-  lda #$00
-  ldx #8
-  cli
-  jsr rtc_read
+; Wrapper functions
+  global _putc
+  global _lcdins
 
 
-  ldx $02 + 0
-  jmp checkthatflag
-
-  ldx $02 + 2
-  txa
-  lsr
-  lsr
-  lsr
-  lsr
-  clc
-  adc #'0'
+; __reg("a") char character
+_putc:
   jsr print_char
-  txa
-  and #$f
-  clc
-  adc #'0'
-  jsr print_char
-  lda #':'
-  jsr print_char
-
-  ldx $02 + 1
-  txa
-  lsr
-  lsr
-  lsr
-  lsr
-  clc
-  adc #'0'
-  jsr print_char
-  txa
-  and #$f
-  clc
-  adc #'0'
-  jsr print_char
-  lda #':'
-  jsr print_char
-
-  ldx $02 + 0
-  txa
-  lsr
-  lsr
-  lsr
-  lsr
-  and #$7
-  clc
-  adc #'0'
-  jsr print_char
-  txa
-  and #$f
-  clc
-  adc #'0'
-  jsr print_char
-  lda #'.'
-checkthatflag:
-  txa
-  and #$80
-  beq .notstop
-  lda #'S'
-  jsr print_char
-  stp
-.notstop:
-
-  lda #%1 ; Reset display
+  rts
+; __reg("a") char instruction
+_lcdins:
   jsr lcd_instruction
-  jmp date_loop
+  rts
 
-done:
-  bra done
-
-
-
-; char defaults[8]
-rtc_default:
-  byte $02 ; Seconds (top bit is CH, clock halt)
-  byte $15
-  byte $18 ; Hours (bit 6 high is 12-hour mode select)
-  byte $01 ; Day of the week?
-  byte $24 ; Day of the month
-  byte $12 ; month
-  byte $22 ; year
-  byte %00010001 ; control register (OUT 0 0 SQWE 0 0 RS1 RS0)
 
   include 4BitLCD.s
