1366af68357fd38cc4d43faca1696cc135e1ff23
Max Heuer <122405954+mrhh69@users.noreply.github.com>
2023-01-21 00:42:34 -0600

    Prepared clock/ subproject with makefiles galore. Still need to get c wrapper functions for lcd/rtc libs.

---
+++ clock/clock.c
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+int main() {
+  /* first, rtc_read to make sure clock is running */
+  /* if not, then re-write rtc defaults */
+
+  for (;;) {
+    /* rtc_read into a buffer */
+    /* print data onto lcd using putc and lcdins */
+  }
+
+  //return 0;
+}
