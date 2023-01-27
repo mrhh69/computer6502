17f0cbdfbf28329a556b68e0d634fe3b3e88559e
Max Heuer <122405954+mrhh69@users.noreply.github.com>
2023-01-27 13:17:22 +0800

    cleaned up code

--- clock/main.s
+++ clock/main.s
@@ -25,8 +25,6 @@
   global interrupt_cb1
 
 ; for main.c
-  global _rtcton
-  global _ntortc
   global _button_states
   global _prev_states
 ; from runner.c
@@ -190,73 +188,6 @@ update_buttons:
 .no_button_edge:
   rts
 
-
-
-
-; overwrites r0 (TODO: make sure that is correct int the vbcc ABI standard)
-; Utility that converts from RTC's format for numbers, to regular int
-; I wrote this and it worked first try (im just that good)
-; Actually, it appeared like it worked first try, but actually had a small bug
-_rtcton:
-  tax
-  and #$0f
-  tay
-  txa
-  and #$f0
-  ; (a >> 4) * 10
-  ; (a >> 4) * 0b1010
-  ; (a >> 4) << 3 + (a >> 4) << 1
-  ; (a >> 1) + (a >> 3)
-  lsr
-  sta r0
-  lsr
-  lsr
-  clc
-  adc r0
-  sta r0
-  tya
-  clc
-  adc r0
-  rts
-
-
-; n into r0
-; kills r1 and r2
-  ; bottom = a % 10
-  ; top = a / 10
-_ntortc:
-; divide
-; r0 -> value (in/out)  -> high nibble
-; r1 -> mod   (out)     -> low nibble
-; r2 -> divisor (const)
-  stz r1
-  lda #10
-  sta r2
-
-  ldx #8
-.divide_loop:
-  rol r0
-  rol r1
-
-  lda r1
-  sec
-  sbc r2
-  bcc .ignore_result
-
-  sta r1
-.ignore_result:
-  dex
-  bne .divide_loop
-
-  rol r0
-
-  lda r0
-  asl
-  asl
-  asl
-  asl
-  ora r1
-  rts
 
 
 
--- i2c_lib/rtc.h
+++ i2c_lib/rtc.h
@@ -6,6 +6,9 @@
 extern void rtc_write(__reg("a/x") char * buf, __reg("r0") unsigned char buf_len, __reg("r1") unsigned char rtc_addr);
 extern void rtc_read (__reg("a/x") char * buf, __reg("r0") unsigned char buf_len, __reg("r1") unsigned char rtc_addr);
 
+extern __reg("a") unsigned char rtcton(__reg("a") unsigned char r);
+extern __reg("a") unsigned char ntortc(__reg("r0") unsigned char n);
+
 
 
 #endif
