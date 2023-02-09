797df0f86e306cc34a5e1cdeb531357b7645026f
Max Heuer <122405954+mrhh69@users.noreply.github.com>
2023-02-09 14:07:35 +0900

    RTC flushes not working for some reason it hangs in either rtc_write or rtc_read (still need to find out which)

--- clock/main.h
+++ clock/main.h
@@ -1,7 +1,16 @@
 
 #ifndef MAIN_H
 #define MAIN_H
+
+// button defs
+#define BUTTON_UP    0x1
+#define BUTTON_DOWN  0x2
+#define BUTTON_LEFT  0x4
+#define BUTTON_RIGHT 0x8
+
 // from main.s
+extern volatile unsigned char button_states;
+extern volatile unsigned char prev_states;
 
 extern void timer2_loop();
 
--- clock/main.s
+++ clock/main.s
@@ -25,28 +25,48 @@
   global interrupt_cb1
 
 ; for main.c
-  global _rtcton
+  global _button_states
+  global _prev_states
 ; from runner.c
   extern _do_init
   extern _do_periodic
+  extern _do_button_press
   extern _mode
 
-; PB6 oscillating at 32768
-TIMER2_COUNT = 32768/8
+; PB6 (->timer2) oscillating at 32768
+; ticks for each loop
+; (buttons updated, handlers called, tick incremented)
+TIMER2_COUNT = (32768/64)
 CURSOR_ON=0
 CURSOR_BLINK=0
 
+; ticks of timer2 runouts until periodic functions are run
+PERIODIC_TICKS = 8
+BUF_FLUSH_TICKS = 64
+; = max(all tick checks)
+MAX_TICKS=BUF_FLUSH_TICKS
+
 NUM_MODES=2
+
+BUTTON_CLR=%01000000; PORTA
+BUTTON_MS =%10000000; PORTA
 
   section text
 
 pre_init:
+  lda #%01000011
+  sta VIA1_DDRA
   lda #(~$40)
   sta VIA1_DDRB
   lda #$ff
-  sta VIA1_DDRA
   sta VIA2_DDRA
   sta VIA2_DDRB
+
+  lda #0
+  sta VIA1_PORTA
+  sta VIA1_PORTB
+  sta VIA2_PORTA
+  sta VIA2_PORTB
 
   lda #(~INT_EN & $ff)
   sta IER
@@ -66,8 +86,18 @@ pre_init:
 
 
 ; Enter the main timer2 loop
+; There are actually two loops happening here
+; -> one, that updates button every timer2 runout
+; -> two, that runs periodic code every PERIODIC_TICKS ticks
 _main:
+; clear button states:
+  lda BUTTON_PORT
+  ora #BUTTON_CLR
+  sta BUTTON_PORT
+  eor #BUTTON_CLR
+  sta BUTTON_PORT
 ; enable and start timer2
+; enable button interrupts
   lda #(T2_COUNTPB6)
   ora ACR
   sta ACR
@@ -77,9 +107,15 @@ _main:
   sta IER
 
 
+  lda #'s'
+  jsr print_char
+
   jsr _rtc_buf_flush
 
   jsr _do_init ; intial mode init
+
+  lda #'d'
+  jsr print_char
 
 .loop:
 ; set flag to 0
@@ -90,21 +126,78 @@ _main:
   sta T2CL
   lda #>TIMER2_COUNT
   sta T2CH
+
+; ----timer2 loop----
 .wai_loop:
   wai
 ; wait for interrupted flag to be set
   lda _timer2_interrupted
   beq .wai_loop
   sei
-; do periodic stuff here:
+; ----do periodic stuff here----
+
+  jsr update_buttons
+
+; check ticks
+  lda _ticks
+  inc
+  and #(BUF_FLUSH_TICKS-1)
+  sta _ticks                ; store because BUF_FLUSH > PERIODIC
+  bne .no_buf_flush
+
+  cli
+  jsr _rtc_buf_flush
+  sei
+.no_buf_flush:
+
+; check ticks (for periodic)
+  lda _ticks
+  and #(PERIODIC_TICKS-1)
+  bne .no_periodic
+
+; do ticks periodic stuff (do_periodic)
+
+
+  cli
+  jsr _do_periodic
+  sei
+
+; show current mode (on top of everything else)
+  lda #($80|15)
+  jsr lcd_instruction
+  lda _mode
+  clc
+  adc #'0'
+  jsr print_char
+.no_periodic:
+
+; ----end periodic stuff----
+  cli
+  bra .loop
+
+
 
 ; check (and update) button states
+update_buttons:
 ; the fact that this is done every 1/8 seconds is good for debouncing
   lda _mode_select_edge ; check MS button
   beq .not_new
-; mode select pressed
+
+; ---- mode select pressed ----
   lda #0
   sta _mode_select_edge
+; update mode select value/edge
+  lda PORTA
+  and #BUTTON_MS
+  beq .low
+.pos:
+; read mode select is 1
+; trigger interrupt on negative edge
+  lda PCR
+  and #~CA1_POS
+  sta PCR
+
+; Only go to next mode on positive edge
 ; next mode (mode = (mode + 1 == NUM_MODES) ? 0 : mode + 1)
   lda _mode
   inc
@@ -113,25 +206,51 @@ _main:
   lda #0
 .nonz:
   sta _mode
+
+  ;lda #1
+  bra .out
+.low:
+; read mode select is 0
+; trigger on positive edge
+  lda PCR
+  ora #CA1_POS
+  sta PCR
+  ;lda #0
+.out:
+  ;sta _mode_select_val
+
+
 ; do init on new mode
+  cli
   jsr _do_init
+  sei
 .not_new:
 
-; mode has been updated, do periodic
-; show current mode
-  lda #($80|15)
-  jsr lcd_instruction
-  lda _mode
-  clc
-  adc #'0'
-  jsr print_char
-
-
-  jsr _do_periodic
+; ---- check other buttons ----
+  lda _button_edge ; UDLR buttons
+  beq .no_button_edge
+; clear interrupt
+  lda BUTTON_PORT
+  ora #BUTTON_CLR
+  sta BUTTON_PORT
+  eor #BUTTON_CLR
+  sta BUTTON_PORT
+; read and store new states:
+  lda _button_states
+  sta _prev_states
+  lda BUTTON_PORT
+  and #(%1111 << 2)
+  lsr
+  lsr
+  sta _button_states
 
   cli
-  jsr _rtc_buf_flush
-  bra .loop
+  jsr _do_button_press
+  sei
+.no_button_edge:
+  rts
+
+
 
 
 interrupt_timer2:
@@ -150,9 +269,16 @@ interrupt_cb1:
   sta _button_edge
   rts
 
+
+
   section bss
 ; flag set after timer2 interrupt
 _timer2_interrupted: reserve 1
+; number of timer2 interrupts
+_ticks: reserve 1
 ; flag set after ca1 interrupt (positive edge)
 _mode_select_edge: reserve 1
+;_mode_select_val: reserve 1
 _button_edge: reserve 1
+_button_states: reserve 1
+_prev_states: reserve 1
